6290314085afeb456678cc9d470592b6ea435a56
bgoncharuck <dariydragon26@gmail.com>
2018-12-17 16:03:28 +0200

    interface implementation

--- ieee754_asInt.js
+++ ieee754_asInt.js
@@ -76,3 +76,89 @@ function bitflag (bit) {
 
 	return 0;
 }
+
+module.exports = {
+	bitflag: (bit) => {
+
+	},
+
+	bitenabled: (num, bit) => {
+
+	},
+
+	bitand: (a, b) => {
+
+	},
+
+	bitor: (a, b) => {
+
+	},
+
+	bitnot (a, b) => {
+
+	},
+
+	bitnotand (a, b) => {
+
+	},
+
+	bitnotor (a, b) => {
+
+	},
+
+	bitxor (a, b) => {
+
+	},
+
+	bitshift_right: (num, size) => {
+
+	},
+
+	bitshift_left: (num, size) => {
+
+	},
+
+	bigger: (what, then) => {
+
+	},
+
+	lesser: (what, then) => {
+
+	},
+
+	equal: (what, to) => {
+
+	},
+
+	biggerequal: (what, then) => {
+
+	},
+
+	lesserequal: (what, then) => {
+
+	},
+
+	compare: (a, b) => {
+
+	},
+
+	outofmax: (to, test) => {
+
+	},
+
+	outofmin: (to, test) => {
+
+	},
+
+	isBaseOfTwo (num) => {
+
+	},
+
+	toBaseOfTwo (num, base) => {
+
+	},
+
+	fromBaseOfTwo (num, base) => {
+
+	}
+};
